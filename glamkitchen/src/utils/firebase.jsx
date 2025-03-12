4c880d2d60bbca43a86ac8d908f2c425677f3aab
Ben <v.skidevelopers@gmail.com>
2025-03-12 13:56:23 +0300

    `Update useProductFunctions to use attribute directly in array-contains query`

--- glamkitchen/src/utils/firebase.jsx
+++ glamkitchen/src/utils/firebase.jsx
@@ -478,7 +478,7 @@ export const useProductFunctions = () => {
       // Correct query to search within the productTags array
       const productsQuery = query(
         productCollectionRef,
-        where("productTags", "array-contains", { [attribute]: true })
+        where("productTags", "array-contains", { attribute: true })
       );
 
       const productsSnapshot = await getDocs(productsQuery);
